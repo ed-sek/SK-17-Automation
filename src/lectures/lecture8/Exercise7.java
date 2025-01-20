16cbc18a8bf41a6b5bf78b0a5f2fdd981f554933
Eduard S. <ed-sek@outlook.com>
2025-01-20 19:09:55 +0200

    Add exercise 4, 5, 6 and 7

---
+++ src/lectures/lecture8/Exercise7.java
@@ -0,0 +1,30 @@
+package lectures.lecture8;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter a string to find its last character: ");
+        String stringToCheck = sc.nextLine();
+
+        char lastCharacterInString = displayLastCharacterOfString(stringToCheck);
+        System.out.println("The last character in string \"" + stringToCheck + "\" is: '" + lastCharacterInString + "'");
+
+        sc.close();
+    }
+
+    // return the last character of a string given by input
+    static char displayLastCharacterOfString(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            System.out.println("String is empty: no last character found.");
+            return ' '; // display empty char and exit the method prematurely
+        }
+        int lastCharacterIndex = inputString.length()-1;
+
+        return inputString.charAt(lastCharacterIndex);
+    }
+}
